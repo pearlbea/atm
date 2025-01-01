b80c914a44ed015e1f8eef9fec2c695aa12b5385
Pearl Latteier <pearlbea@gmail.com>
2025-01-01 09:36:53 -0600

    Wrap input in form element to allow submission via keyboard

--- ui/src/components/SignIn.tsx
+++ ui/src/components/SignIn.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import {FormEvent, useState} from 'react';
 import {Paper, Grid, TextField, Button} from '@mui/material';
 
 type SignInProps = {
@@ -10,35 +10,42 @@ export const SignIn = (props: SignInProps) => {
   const {signIn, accountNumberError} = props;
   const [accountNumber, setAccountNumber] = useState('');
 
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    await signIn(+accountNumber);
+  }
+
   return (
-    <Paper sx={{ border: 20, borderBottom: 30, borderColor: 'white' }}> 
-            <h1 className='app-title'>Please Sign in with your Account Number:</h1>
-            <Grid container>
-              <Grid item xs={2} />
-              <Grid item xs={8}>
-                <TextField 
-                  type='number' 
-                  variant='outlined' 
-                  label='Account Number' 
-                  sx={{ display: 'flex', margin: 'auto' }}
-                  onChange={(e) => setAccountNumber(e.target.value)}
-                  error={accountNumberError}
-                />
-              </Grid>
-              <Grid item xs={2} />
-            </Grid>
-            <Button 
-              variant='contained' 
-              color='primary' 
-              sx={{
-                display: 'block',
-                margin: 'auto',
-                marginTop: 2,
-              }}
-              onClick={async() => await signIn(+accountNumber)}
-            >
-              Sign In
-            </Button>
-          </Paper>
-  )
+    <Paper sx={{ border: 20, borderBottom: 30, borderColor: "white" }}>
+      <form onSubmit={handleSubmit}>
+        <h1 className="app-title">Please Sign in with your Account Number:</h1>
+        <Grid container>
+          <Grid item xs={2} />
+          <Grid item xs={8}>
+            <TextField
+              type="number"
+              variant="outlined"
+              label="Account Number"
+              sx={{ display: "flex", margin: "auto" }}
+              onChange={(e) => setAccountNumber(e.target.value)}
+              error={accountNumberError}
+            />
+          </Grid>
+          <Grid item xs={2} />
+        </Grid>
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          sx={{
+            display: "block",
+            margin: "auto",
+            marginTop: 2,
+          }}
+        >
+          Sign In
+        </Button>
+      </form>
+    </Paper>
+  );
 }
